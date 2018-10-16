03eed547705da1b43ef701f69442999056c28827
github username <github email address>
2018-10-16 12:06:38 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+const MakeMovies = ({movies}) => {
+  return movies.map(movie => <h3>{movie}</h3>)
 }
+
+const MakeDirectors = ({theTalent}) => {
+  return theTalent.map(director => {
+    return(
+      <div className='director'>
+        <h2>{director.name}</h2>
+        <MakeMovies movies={director.movies} />
+      </div>
+    )
+  })
+}
+
+const Directors = () => {
+  return(
+        <div>
+          <h1>Directors Page</h1>
+          <MakeDirectors theTalent={directors}/>
+        </div>
+  );
+};
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { movies } from '../data';
 
+const MakeMovies = ({movies}) => {
+  return movies.map(movie => <h3>{movie}</h3>)
+}
+
+const MakeDirectors = ({theThing}) => {
+  return theThing.map(movie => {
+    return(
+      <div className='movie'>
+        <h2>{movie.name}</h2>
+        <h2>{movie.time}</h2>
+        <h2>{movie.metascore}</h2>
+        <MakeMovies movies={movie.genres} />
+      </div>
+    )
+  })
+}
+
 const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
+  return(
+        <div>
+          <h1>Movies Page</h1>
+          <MakeDirectors theThing={movies}/>
+        </div>
   );
 };
 
